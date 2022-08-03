e2adc209bb59f7a2c7faabd7ac3acb49e6d06f39
Luyn <xxxtrglyn@gmail.com>
2022-08-03 13:49:58 +0700

    Create project

---
+++ src/main/java/com/example/vodkabulary/VodkabularyApplication.java
@@ -0,0 +1,13 @@
+package com.example.vodkabulary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VodkabularyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VodkabularyApplication.class, args);
+	}
+
+}
