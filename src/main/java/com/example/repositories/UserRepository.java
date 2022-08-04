ca4ef7597a04d65168926c3b327c9f2c0d9f2c38
Luyn <xxxtrglyn@gmail.com>
2022-08-04 10:02:42 +0700

    luyndeptrai

--- src/main/java/com/example/repositories/UserRepository.java
+++ src/main/java/com/example/repositories/UserRepository.java
@@ -1,4 +1,6 @@
 package com.example.repositories;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository {
 }
